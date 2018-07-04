07afffd61490634422d4eaf18e93d6ee82a8c0d6
Pavel Kuznetsov <mig38m@gmail.com>
2018-07-05 00:10:17 +0500

    Create List.js

---
+++ react/Player/playlist/List.js
@@ -0,0 +1,19 @@
+// essentials
+import React from 'react'
+import { connect } from 'react-redux'
+
+import Item from './Item'
+
+const items = arr => (
+    arr.map(i => <Item key={i} name={i} />)
+)
+
+const List = ({ arr }) => (
+    <div className='list'>
+        { items(arr) }
+    </div>
+)
+
+const mapStateToProps = store => ({ arr: store.playlists })
+
+export default connect(mapStateToProps)(List)
