aae147d1f4df941fbf069260169fce887afc8f8a
Pavel Kuznetsov <mig38m@gmail.com>
2018-07-05 00:06:12 +0500

    Create DisplayProgress.js

---
+++ react/Player/DisplayProgress.js
@@ -0,0 +1,27 @@
+// essentials
+import React from 'react'
+import { connect } from 'react-redux'
+
+const DisplayProgress = ({position, total}) => {
+    const percent = position / total
+    const len = (percent) ? `${percent * 100}%` : `0`
+    return (
+        <svg xmlns="http://www.w3.org/svg/2000"
+            className='display__progress'>
+            <line
+                x1={0}
+                x2={len}
+                y1='50%'
+                y2='50%'
+                />
+        </svg>
+    )
+}
+
+const mapStateToProps = store => ({
+    position: Number(store.playerPosition[0]),
+    total: Number(store.playerPosition[1])
+})
+
+
+export default connect(mapStateToProps)(DisplayProgress)
