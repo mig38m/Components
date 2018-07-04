997e1f89498a5d12639a6a229afe407263496162
Pavel Kuznetsov <mig38m@gmail.com>
2018-07-04 23:31:35 +0500

    Create About.js

---
+++ react/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Card, CardTitle, CardText, CardActions, Button } from 'react-mdl';
+import { Link } from 'react-router-dom';
+import { VERSION } from '../constants';
+
+export default () => (
+  <Card shadow={0} style={{ margin: 'auto', textAlign: 'center' }}>
+    <CardTitle style={{ margin: 'auto' }}>Remember</CardTitle>
+    <CardText>
+      Version {VERSION}<br/><br/>
+      <a href="https://github.com/paulhoughton/remember/">Github</a>
+    </CardText>
+    <CardActions border>
+      <Link to="/"><Button colored>OK</Button></Link>
+    </CardActions>
+  </Card>
+);
