c020da61b47ce750766d7e8df1a5f3fedc048372
Pavel Kuznetsov <mig38m@gmail.com>
2018-07-05 00:44:06 +0500

    Create Volume.js

---
+++ react/Player/Controls/Volume.js
@@ -0,0 +1,50 @@
+// essentials
+import React from 'react'
+import { connect } from 'react-redux'
+
+import debounce from '../../debounce'
+
+const Volume = React.createClass({
+
+    getInitialState() {
+        return {
+            val: 100
+        }
+    },
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            val: nextProps.volume
+        })
+    },
+
+    emitVolume() {
+        this.props.socket.emit('updateVolume', this.state.val)
+    },
+
+    updateVolume(event) {
+
+        this.setState({ val: event.target.value })
+
+        debounce(this.emitVolume, 500)()
+
+    },
+
+    render() {
+        return (
+            <input
+                type='range'
+                min={0}
+                max={100}
+                value={this.state.val}
+                onChange={this.updateVolume} />
+        )
+    }
+})
+
+const mapStateToProps = store => ({
+    socket: store.socket,
+    volume: store.volume
+})
+
+export default connect(mapStateToProps)(Volume)
