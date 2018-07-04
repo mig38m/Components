35c6387e270f85f494f443ceb5935a0ea51c3e83
Pavel Kuznetsov <mig38m@gmail.com>
2018-07-04 23:59:42 +0500

    Create NextTrack.js

---
+++ react/NextTrack.js
@@ -0,0 +1,21 @@
+// essentials
+import React from 'react'
+import { connect } from 'react-redux'
+
+// icon component
+import MdSkipNext from 'react-icons/lib/md/skip-next'
+
+const playNext = socket => () => {
+    socket.emit('playNextTrack')
+    console.log('Playing next track')
+}
+
+const Next = ({socket}) => (
+    <div className="icon-wrapper" onClick={ playNext(socket) }>
+        <MdSkipNext />
+    </div>
+)
+
+const mapStateToProps = store => ({ socket: store.socket })
+
+export default connect(mapStateToProps)(Next)
