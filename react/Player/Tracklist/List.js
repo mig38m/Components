c19e50a4f5c8211d791d195852acbbd78ae6e472
Pavel Kuznetsov <mig38m@gmail.com>
2018-07-05 00:31:12 +0500

    Create List.js

---
+++ react/Player/Tracklist/List.js
@@ -0,0 +1,25 @@
+// essentials
+import React from 'react'
+import { connect } from 'react-redux'
+
+import Item from './Item'
+
+const items = arr => (
+    arr[0].map((item, index) => (
+        <Item
+            key={item}
+            name={item}
+            time={arr[1][index]}
+            />
+    ))
+)
+
+const List = ({arr}) => (
+    <div className='list'>
+        {items(arr) }
+    </div>
+)
+
+const mapStateToProps = store => ({ arr: store.tracksData })
+
+export default connect(mapStateToProps)(List)
