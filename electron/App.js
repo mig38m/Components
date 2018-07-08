127714a5e717e27ae4c0236944296354f771b041
Pavel Kuznetsov <mig38m@gmail.com>
2018-07-08 16:03:17 +0500

    Create App.js

---
+++ electron/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import TodoBoard from './TodoBoard';
+
+class App extends Component {
+    render() {
+        return (
+            <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+                <TodoBoard
+                    url='http://localhost:8080/api/todos'
+                    pollInterval={2000} />
+            </MuiThemeProvider>
+        );
+    }
+}
+
+export default App;
