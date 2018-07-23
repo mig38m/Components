09d8cb6ccc7f62d86ffb811f347a26c9cec1d8ae
Pavel Kuznetsov <mig38m@gmail.com>
2018-07-23 13:22:08 +0500

    Create HNBot.js

---
+++ logic/HNBot.js
@@ -0,0 +1,49 @@
+var request = require('request');
+var async = require('async');
+var https = require('https');
+
+var BASE_URL = 'https://news.ycombinator.com/';
+
+
+module.exports = {
+
+  aliases: ['hn', 'hackernews', 'hacker news'],
+
+  pingAndSend: function (callback) {
+
+    request('http://node-hnapi.herokuapp.com/news', function(err, response, items) {
+      if (err || response.statusCode != 200) {
+        callback(err);
+      }
+
+      items = JSON.parse(items);
+
+      var botPayload = 'Current Hacker News homepage:\n'
+
+      var index = 0;
+      async.forEach(items, function(item, cb) {
+        var url = item.url;
+        if (item.url.indexOf('http') !== 0) {
+          url = BASE_URL + item.url
+        }
+
+        botPayload += '<%url%|%rank%. %title%>\n'
+                        .replace('%url%', url)
+                        .replace('%rank%', index + 1)
+                        .replace('%title%', item.title);
+        index++;
+        cb();
+      }, function(err) {
+
+        var payload = {
+          text: botPayload,
+          username: 'HackerNews'
+        };
+
+        callback(null, payload);
+      });
+    });
+
+  }
+
+}
